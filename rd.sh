e9689df46f8f5efaa08dac8ea88c39d9b9e5f332
WhyQwerty23811 <94951817+WhyQwerty23811@users.noreply.github.com>
2024-10-28 19:57:23 +0700

    Update rd.sh

--- rd.sh
+++ rd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AVG7fiS6Rg7PBT9K32J4ObvUdq8kUM0MWGLZx4DpRIngBX-AFmSS2GRia-o4HFH27Wbb7w" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --user-name=hxler
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AVG7fiSpBUXvOFeKw1Xkp7XbUkJ2EW545mycJUWK23k1IKJE0krf2gOLvgCPDKcA2Mje4A" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --user-name=hxler
