a8f1de49c52a3afc279dc9cb526ff5f79ff7cd9d
WhyQwerty23811 <94951817+WhyQwerty23811@users.noreply.github.com>
2024-10-29 11:07:41 +0700

    Update rd.sh

--- rd.sh
+++ rd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/gnome-session" > /etc/chrome-remote-desktop-session' && cat /etc/chrome-remote-desktop-session systemctl --user stop pulseaudio /usr/bin/pulseaudio --start exec /etc/X11/Xsession /usr/bin/gnome-session
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AVG7fiTynrLy6HfFOhUIwWnHDTADENHbj6Bfer_7ctD5B1iP-rVD3vM8QQHp3a4VesEJLA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AVG7fiTwT1j1JYnXKKWZaxzkZEnXr529KwQcMuKXxvWlHANMpcoVvBabl3rN1xHQ7RsHBA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
